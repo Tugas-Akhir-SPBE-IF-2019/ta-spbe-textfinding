67fd1643d687fdd6e21cce4442844c458f5aeb92
akramab <13519142@std.stei.itb.ac.id>
2023-03-14 20:35:16 +0700

    feat: add text finding for indicator 8

---
+++ textfinding_indikator8.py
@@ -0,0 +1,74 @@
+# this code is for TEXT FINDING - INDIKATOR 8
+# input is txt file from preprocess dokumen lama, nama instansi & judul from dokumen lama & baru
+
+import re
+import preprocess_dokbaru as dokbaru
+import preprocess_doklama as doklama
+
+
+def txtreader(filename, lv, keyword):
+    # func to search keyword in txt file
+
+    # open txt file
+    file = open(f'cleaned_{filename}.txt', 'r')
+
+    idx = 0
+    result = []
+
+    # read line by line from txt
+    for line in file:
+
+        # text finding goes straight to lvl 2 because all documents are lvl 1 by default
+        if (lv == 2):
+            # check using 
+            for key in keyword:
+
+                reg = f'{key}'
+                if re.search(reg, line, re.IGNORECASE):
+                    result.append([idx, line])
+
+        idx += 1
+
+    file.close()
+    return (result)
+
+
+# if __name__ == '__main__':
+#     pdfparser(sys.argv[1])
+
+
+# for indicator 8, ceklvl will only check against lvl2 keywords only
+# because the rest are related to lvl2 and will be checked in the next step (text similarity)
+def ceklvl(filename):
+    list_final = []
+    text_final = ''
+
+    lvl2 = ["manajemen keamanan informasi"]
+    res2 = txtreader(filename, 2, lvl2)
+
+    # check if keyword lvl2 is not found, then return as empty string
+    if (not res2):
+        return ''
+
+    for el in res2:
+        list_final.append(el[1])
+
+    text_final = ". ".join(list_final)
+
+    # clean text
+    text_final = re.sub(r'(\n)+', '', text_final, flags=re.MULTILINE)
+    text_final = re.sub(r'(;)+', ',', text_final, flags=re.MULTILINE)
+
+    return text_final
+
+
+filename = 'F2201-287-Indikator_01_+_Indikator1_Perbup_81_tahun_2021.pdf'
+
+(instansibaru, judulbaru) = dokbaru.pdfparser(filename)
+(instansilama, judullama) = doklama.pdfparser(filename)
+
+teks_final = ceklvl(filename)
+
+# hasil text finding
+# bisa langsung digunakan ke model text similarity
+print(teks_final)
