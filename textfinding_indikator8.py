80f628cf1bdc054a108479fd13794248d92a2bea
akramab <13519142@std.stei.itb.ac.id>
2023-03-19 11:04:35 +0700

    fix: refactor code to use utility functions and fix some logics

--- textfinding_indikator8.py
+++ textfinding_indikator8.py
@@ -4,6 +4,7 @@
 import re
 import preprocess_dokbaru as dokbaru
 import preprocess_doklama as doklama
+from utility import *
 
 
 def txtreader(filename, lv, keyword):
@@ -14,6 +15,7 @@ def txtreader(filename, lv, keyword):
 
     idx = 0
     result = []
+    initial_keys = list(keyword)
 
     # read line by line from txt
     for line in file:
@@ -22,20 +24,30 @@ def txtreader(filename, lv, keyword):
             # check using 
             for key in keyword:
 
-                reg = f'{key}'
-                if re.search(reg, line, re.IGNORECASE):
+                if re.search(key, line, re.IGNORECASE):
                     result.append([idx, line])
+
+        elif (lv == 3):
+            # Check primary word first:
+            reg = f'(?:(manajemen)\s+(keamanan)\s+(informasi))'
+
+            if (re.search(reg, line, re.IGNORECASE)):
+                for key in initial_keys:
+                    if (re.search(key, line, re.IGNORECASE)):
+                        result.append([idx, line])
+
+                        # Delete found key
+                        if (key in keyword):
+                            keyword.remove(key)
         
-        else: #lv 2,3, and 4 should follow the same pattern
-            # TODO 
-            # this still doesn't feel right to me; subject for later change
-            reg = f'manajemen keamanan informasi'
+        else: #lv 4
+            reg = f'(?:(manajemen)\s+(keamanan)\s+(informasi))'
 
             if (re.search(reg, line, re.IGNORECASE)):
                 # check if line contains any keyword from list
-                res = [ele for ele in keyword if (ele in line)]
-                if (res):
-                    result.append([idx, line])
+                for key in keyword:
+                    if (re.search(key, line, re.IGNORECASE)):
+                        result.append([idx, line])
 
         idx += 1
 
@@ -46,14 +58,10 @@ def txtreader(filename, lv, keyword):
 # if __name__ == '__main__':
 #     pdfparser(sys.argv[1])
 
-
-# for indicator 8, ceklvl will only check against lvl2 keywords only
-# because the rest are related to lvl2 and will be checked in the next step (text similarity)
 def ceklvl(filename):
     list_final = []
-    text_final = ''
 
-    lvl1 = ["manajemen keamanan informasi"]
+    lvl1 = convert_keywords(["manajemen keamanan informasi"])
     res1 = txtreader(filename, 1, lvl1)
 
     # check if keyword lvl1 is not found, then return as empty string
@@ -64,40 +72,41 @@ def ceklvl(filename):
         list_final.append(el[1])
 
     # keywords for level 2 and 3 are the same
-    lvl2_3 = ['penetapan ruang lingkup', 
+    lvl2_3 = convert_keywords(['penetapan ruang lingkup', 
               'penetapan penanggung jawab', 
               'perencanaan', 
               'dukungan pengoperasian',
               'evaluasi kinerja',
-              'perbaikan berkelanjutan terhadap Keamanan Informasi']
-    res2_3 = txtreader(filename, 2, lvl2_3)
+              'perbaikan berkelanjutan terhadap Keamanan Informasi'])
+    res2_3 = txtreader(filename, 3, lvl2_3)
 
     for el in res2_3:
         if (el[1] not in list_final):
             list_final.append(el[1])
+
+    # check if there are still any keywords remain (meaning that lvl 3 is not fulfilled)
+    # immediately return if any keywords remain
+    if (lvl2_3):
+        return clean_text(list_final)
     
-    lvl4 = ['setiap opd', 
+    lvl4 = convert_keywords(['setiap opd', 
            'seluruh opd', 
            'setiap unit kerja', 
            'seluruh unit kerja', 
            'setiap pemerintah daerah',
-           'seluruh pemerintah daerah']
+           'seluruh pemerintah daerah'])
     res4 = txtreader(filename, 4, lvl4)
 
     for el in res4:
         if (el[1] not in list_final):
             list_final.append(el[1])
 
-    text_final = ". ".join(list_final)
 
-    # clean text
-    text_final = re.sub(r'(\n)+', '', text_final, flags=re.MULTILINE)
-    text_final = re.sub(r'(;)+', ',', text_final, flags=re.MULTILINE)
-
-    return text_final
+    return clean_text(list_final)
 
 
-filename = 'F2201-287-Indikator_01_+_Indikator1_Perbup_81_tahun_2021.pdf'
+# filename = 'F2201-287-Indikator_01_+_Indikator1_Perbup_81_tahun_2021.pdf'
+filename = 'PERBUB NO 34 TAHUN 2019 TENTANG RENCANA INDUK SPBE.pdf'
 
 (instansibaru, judulbaru) = dokbaru.pdfparser(filename)
 (instansilama, judullama) = doklama.pdfparser(filename)
