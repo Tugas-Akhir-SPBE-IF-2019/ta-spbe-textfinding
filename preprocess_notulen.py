6df2a593cfa900e1acc061d4d2a62c8455280832
afifahfq <afifahfq@gmail.com>
2023-05-29 11:01:12 +0700

    Squashed commit of the following:
    commit dbda0d9f0f5f15e2d565dcce68e3dbfd517222e3
    Author: Afifah Fathimah Q <68496334+afifahfq@users.noreply.github.com>
    Date:   Wed May 17 08:37:43 2023 +0700
    
        Create preprocess_notulen.py (#10)
    
    commit 60a3ab50858af468c1858052b44d7d2b513a65ff
    Author: Afifah Fathimah Q <68496334+afifahfq@users.noreply.github.com>
    Date:   Wed May 10 20:26:54 2023 +0700
    
        Feat/indikator 9 (#9)
    
        * Create textfinding_indikator9.py
    
        * Update textfinding_indikator9.py
    
        * del removal

---
+++ preprocess_notulen.py
@@ -0,0 +1,67 @@
+# this code is for DOKUMEN BUKTI LAMA
+# the code below will get the title
+
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+import io
+import re
+
+
+def pdfparser(data):
+
+    fp = open(data, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    page_no_title = 0
+
+    for pageNumber, page in enumerate(PDFPage.get_pages(fp)):
+        interpreter.process_page(page)
+        data = retstr.getvalue()
+        retstr.truncate(0)
+        # retstr.seek(0)
+
+        if (pageNumber == page_no_title):
+            # get perihal dokumen
+            perihal = re.split('(?i)perihal', data)[1]
+            perihal = stringcleaner(perihal)
+
+            # if perihal is generalized
+            if (perihal.lower() in ['undangan', 'notulensi', 'notulen']):
+                perihal = re.split('(?i)keperluan', data)[1]
+                perihal = stringcleaner(perihal)
+
+        break
+
+    return (perihal)
+
+
+def stringcleaner(data):
+    codec = 'utf-8'
+
+    # clean leading whitespace
+    data = re.split('\n+', data)[1]
+    print(data)
+
+    # split on new line
+    data = re.split('\n+', data)[0]
+
+    return data.strip()
+
+
+# if __name__ == '__main__':
+#     pdfparser(sys.argv[1])
+
+filename = 'Nota Dinas.pdf'
+
+perihal = pdfparser(filename)
+print(perihal)
---
+++ textfinding_indikator9.py
@@ -0,0 +1,85 @@
+# this code is for TEXT FINDING - INDIKATOR 9
+# input is txt file from preprocess dokumen lama, nama instansi & judul from dokumen lama & baru
+
+import re
+import preprocess_dokbaru as dokbaru
+import preprocess_doklama as doklama
+from utility import *
+
+
+def txtreader(filename, lv, keyword):
+    # func to search keyword in txt file
+
+    # open txt file
+    file = open(f'cleaned_{filename}.txt', 'r')
+
+    idx = 0
+    result = []
+
+    # read line by line from txt
+    for line in file:
+
+        if (lv == 1):
+            for key in keyword:
+                reg = f'(?:(audit)\s({key}))'
+                if (re.search(reg, line, re.IGNORECASE)):
+                    result.append([idx, line])
+
+        else:  # lv == 2
+            # cek dgn keyword 'audit'
+            for key in keyword:
+
+                # manfaatin regex untuk searching yg not only kata keyword
+                reg = f'(?:(audit)\s({key}))'
+                if (re.search(reg, line, re.IGNORECASE)):
+                    result.append([idx, line])
+
+        idx += 1
+
+    file.close()
+    return (result)
+
+
+# if __name__ == '__main__':
+#     pdfparser(sys.argv[1])
+
+def ceklvl(filename):
+    list_final = []
+    text_final = ''
+
+    lvl1 = ["TIK", "Teknologi Informasi dan Komunikasi"]
+    res1 = txtreader(filename, 1, lvl1)
+
+    # cek if keyword lvl1 is not found, then return as empty string
+    if (not res1):
+        return text_final
+
+    lvl2 = convert_keywords(
+        ["Infrastruktur SPBE", "Aplikasi SPBE", "Keamanan SPBE"])
+    res2 = txtreader(filename, 2, lvl2)
+
+    if (not res2):
+        list_final.append(res1[0][1])
+        return clean_text(list_final)
+
+    for el in res2:
+        if (el[1] not in list_final):
+            list_final.append(el[1])
+
+    text_final = ". ".join(list_final)
+
+    return clean_text(list_final)
+
+
+filename = 'F2201-287-Indikator_01~+~Indikator1_Perbup_81_tahun_2021.pdf'
+# filename2 = 'Draft Perbup SPBE 2021 Revisi.pdf'
+
+(instansibaru, judulbaru) = dokbaru.pdfparser(filename)
+(instansilama, judullama) = doklama.pdfparser(filename)
+teks_final = ceklvl(filename)
+
+# hasil text finding
+# bisa langsung digunakan ke model text similarity
+print(teks_final)
+print(instansibaru, judulbaru)
+print(instansilama, judullama)
